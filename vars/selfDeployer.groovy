81be527e87ade679079c36a09332a0806f8dee4b
PRAMOD KUMAR <114461664+PK366@users.noreply.github.com>
2025-04-11 18:26:04 +0530

    Update selfDeployer.groovy
    //

--- vars/selfDeployer.groovy
+++ vars/selfDeployer.groovy
@@ -14,11 +14,11 @@ spec:
     - cat
     tty: true
     env:  
-      - name: "GOOGLE_APPLICATION_CREDENTIALS"
-        value: "/var/run/secret/cloud.google.com/service-account.json"              
+      // - name: "GOOGLE_APPLICATION_CREDENTIALS"
+      //   value: "/var/run/secret/cloud.google.com/service-account.json"              
     volumeMounts:
-      - name: service-account
-        mountPath: /var/run/secret/cloud.google.com
+      // - name: service-account
+      //   mountPath: /var/run/secret/cloud.google.com
       - name: kube-config
         mountPath: /root/.kube     
     resources:
@@ -29,14 +29,14 @@ spec:
         memory: "320Mi"
         cpu: "300m"  
   volumes:
-  - name: service-account
-    projected:
-      sources:
-      - secret:
-          name: jenkins-credentials
-          items:
-            - key: gcpKmsServiceAccount
-              path: service-account.json   
+  // - name: service-account
+  //   projected:
+  //     sources:
+  //     - secret:
+  //         name: jenkins-credentials
+  //         items:
+  //           - key: gcpKmsServiceAccount
+  //             path: service-account.json   
   - name: kube-config
     secret:
         secretName: "${env.ENVIRONMENTS}-kube-config"                    
