a820233be7c5bd148523120d14139d88fde4b486
PRAMOD KUMAR <114461664+PK366@users.noreply.github.com>
2025-04-18 15:19:22 +0530

    Update buildPipeline.groovy new fle

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -73,7 +73,7 @@ spec:
             String serviceCategory = null;
             String buildNum = null;
 
-            for(int i=0; i<jobConfigs.size(); i++){
+            for (int i = 0; i < jobConfigs.size(); i++) {
                 JobConfig jobConfig = jobConfigs.get(i)
 
                 stage('Parse Latest Git Commit') {
@@ -81,91 +81,96 @@ spec:
                              "PATH=alpine:$PATH"
                     ]) {
                         container(name: 'git', shell: '/bin/sh') {
-                            scmVars['VERSION'] = sh (script:
-                                    '/scripts/get_application_version.sh ${BUILD_PATH}',
-                                    returnStdout: true).trim()
-                            scmVars['ACTUAL_COMMIT'] = sh (script:
-                                    '/scripts/get_folder_commit.sh ${BUILD_PATH}',
-                                    returnStdout: true).trim()
+                            scmVars['VERSION'] = sh(script: '/scripts/get_application_version.sh ${BUILD_PATH}', returnStdout: true).trim()
+                            scmVars['ACTUAL_COMMIT'] = sh(script: '/scripts/get_folder_commit.sh ${BUILD_PATH}', returnStdout: true).trim()
                             scmVars['BRANCH'] = scmVars['GIT_BRANCH'].replaceFirst("origin/", "")
                         }
                     }
                 }
 
                 stage('Build with Kaniko') {
-                    withEnv(["PATH=/busybox:/kaniko:$PATH"
-                    ]) {
+                    withEnv(["PATH=/busybox:/kaniko:$PATH"]) {
                         container(name: 'kaniko', shell: '/busybox/sh') {
 
-                            for(int j=0; j<jobConfig.getBuildConfigs().size(); j++){
+                            for (int j = 0; j < jobConfig.getBuildConfigs().size(); j++) {
                                 BuildConfig buildConfig = jobConfig.getBuildConfigs().get(j)
-                                echo "${buildConfig.getWorkDir()} ${buildConfig.getDockerFile()}"
-                                if( ! fileExists(buildConfig.getWorkDir()) || ! fileExists(buildConfig.getDockerFile()))
-                                    throw new Exception("Working directory / dockerfile does not exist!");
+                                echo "Preparing to build image for: ${buildConfig.getImageName()}"
+                                echo "Checking paths:"
+                                echo "WorkDir: ${buildConfig.getWorkDir()}"
+                                echo "Dockerfile: ${buildConfig.getDockerFile()}"
+                                
+                                sh "ls -al ${buildConfig.getWorkDir()} || echo 'WorkDir not found!'"
+                                sh "ls -al ${buildConfig.getDockerFile()} || echo 'Dockerfile not found!'"
+
+                                if (!fileExists(buildConfig.getWorkDir()) || !fileExists(buildConfig.getDockerFile())) {
+                                    throw new Exception("Working directory or Dockerfile does not exist!")
+                                }
 
                                 String workDir = buildConfig.getWorkDir().replaceFirst(getCommonBasePath(buildConfig.getWorkDir(), buildConfig.getDockerFile()), "./")
-                                String image = null;
-                                if(scmVars.BRANCH.equalsIgnoreCase("master")) {
-                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:v${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}";
+                                String image
+                                if (scmVars.BRANCH.equalsIgnoreCase("master")) {
+                                    image = "${REPO_NAME}/${buildConfig.getImageName()}:v${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}"
                                 } else {
-                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:${scmVars.BRANCH}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}";
-                                } 
-                                serviceCategory = buildConfig.getServiceCategoryName();  // Dashboard
-                                buildNum = "${scmVars.VERSION}"; // Dashboard
-                                String noPushImage = env.NO_PUSH ? env.NO_PUSH : false;
-                                echo "ALT_REPO_PUSH ENABLED: ${ALT_REPO_PUSH}"
-                                 if(env.ALT_REPO_PUSH.equalsIgnoreCase("true")){
-                                  String gcr_image = "${GCR_REPO_NAME}/${buildConfig.getImageName()}:${env.BUILD_NUMBER}-${scmVars.BRANCH}-${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}";
-                                  sh """
-                                    echo \"Attempting to build image,  ${image}\"
-                                    /kaniko/executor -f `pwd`/${buildConfig.getDockerFile()} -c `pwd`/${buildConfig.getContext()} \
-                                    --build-arg WORK_DIR=${workDir} \
-                                    --build-arg token=\$GIT_ACCESS_TOKEN \
-                                    --cache=true --cache-dir=/cache \
-                                    --single-snapshot=true \
-                                    --snapshotMode=time \
-                                    --destination=${image} \
-                                    --destination=${gcr_image} \
-                                    --no-push=${noPushImage} \
-                                    --force \
-                                    --cache-repo=pk366/cache/cache
-                                  """  
-                                  echo "${image} and ${gcr_image} pushed successfully!!"                              
+                                    image = "${REPO_NAME}/${buildConfig.getImageName()}:${scmVars.BRANCH}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}"
                                 }
-                                else{
-                                sh """
-                                    echo \"Attempting to build image,  ${image}\"
+                                
+                                serviceCategory = buildConfig.getServiceCategoryName()
+                                buildNum = "${scmVars.VERSION}"
+                                String noPushImage = env.NO_PUSH ? env.NO_PUSH : "false"
+                                
+                                echo "NO_PUSH=${noPushImage}"
+                                echo "ALT_REPO_PUSH=${env.ALT_REPO_PUSH ?: 'not set'}"
+
+                                if (env.ALT_REPO_PUSH?.equalsIgnoreCase("true")) {
+                                    String gcr_image = "${GCR_REPO_NAME}/${buildConfig.getImageName()}:${env.BUILD_NUMBER}-${scmVars.BRANCH}-${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}"
+                                    echo "Kaniko build with GCR push for image: ${image} and ${gcr_image}"
+
+                                    sh """
                                     /kaniko/executor -f `pwd`/${buildConfig.getDockerFile()} -c `pwd`/${buildConfig.getContext()} \
-                                    --build-arg WORK_DIR=${workDir} \
-                                    --build-arg token=\$GIT_ACCESS_TOKEN \
-                                    --cache=true --cache-dir=/cache \
-                                    --single-snapshot=true \
-                                    --snapshotMode=time \
-                                    --destination=${image} \
-                                    --no-push=${noPushImage} \
-                                    --force \
-                                    --cache-repo=pk366/cache/cache
-                                """
-                                echo "${image} pushed successfully!"
-                                }                                
+                                        --build-arg WORK_DIR=${workDir} \
+                                        --build-arg token=\$GIT_ACCESS_TOKEN \
+                                        --cache=true --cache-dir=/cache \
+                                        --single-snapshot=true \
+                                        --snapshotMode=time \
+                                        --destination=${image} \
+                                        --destination=${gcr_image} \
+                                        --no-push=${noPushImage} \
+                                        --force \
+                                        --cache-repo=pk366/cache/cache \
+                                        2>&1 | tee kaniko_build_output.log
+                                    """
+                                    echo "${image} and ${gcr_image} pushed successfully!"
+                                } else {
+                                    echo "Kaniko build for image: ${image}"
+
+                                    sh """
+                                    /kaniko/executor -f `pwd`/${buildConfig.getDockerFile()} -c `pwd`/${buildConfig.getContext()} \
+                                        --build-arg WORK_DIR=${workDir} \
+                                        --build-arg token=\$GIT_ACCESS_TOKEN \
+                                        --cache=true --cache-dir=/cache \
+                                        --single-snapshot=true \
+                                        --snapshotMode=time \
+                                        --destination=${image} \
+                                        --no-push=${noPushImage} \
+                                        --force \
+                                        --cache-repo=pk366/cache/cache \
+                                        2>&1 | tee kaniko_build_output.log
+                                    """
+                                    echo "${image} pushed successfully!"
+                                }
                             }
                         }
                     }
                 }
+                // Uncomment to enable dashboard updates
                 // stage ("Update dashboard") {
-                //         environmentDashboard {
-                //             environmentName(scmVars.BRANCH)  
-                //             componentName(serviceCategory)
-                //             buildNumber(buildNum)
-                //             //buildJob(String buildJob)
-                //             //packageName(String packageName)
-                //             //addColumns(true)
-                //             //Date now = new Date()                                
-                //             //columns(String Date, now.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))) 
+                //     environmentDashboard {
+                //         environmentName(scmVars.BRANCH)  
+                //         componentName(serviceCategory)
+                //         buildNumber(buildNum)
                 //     }    
-                // }    
+                // }
             }
         }
     }
-
 }
