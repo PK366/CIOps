d283d4733d1d82fdef1545413f3224e8c9733b6c
PRAMOD KUMAR <114461664+PK366@users.noreply.github.com>
2025-04-18 16:13:46 +0530

    Update jobBuilder.groovy old

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -29,7 +29,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: sparrowsoftech
+        value: pk366
       - name: DOCKER_GROUP_NAME  
         value: dev
     resources:
